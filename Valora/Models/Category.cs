d3bc2cb16d9356adc46b881b40d267d76b6d6907
MostafaBadr55 <mosstafabadr@gmail.com>
2025-11-11 09:43:58 +0200

    adding Category model and modify product model

---
+++ Valora/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Valora.Models
+{
+    public class Category: BaseModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [ForeignKey("Products")]
+        public int ProductId { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
--- Valora/Models/Product.cs
+++ Valora/Models/Product.cs
@@ -1,7 +1,16 @@
-﻿namespace Valora.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Valora.Models
 {
     public class Product: BaseModel
     {
-
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int StockQuantity { get; set; }
+        public string ImgUrl { get; set; }
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
