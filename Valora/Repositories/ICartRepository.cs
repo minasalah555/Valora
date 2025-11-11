7c401667eca98b77fa0cc11a3a1425ab9720fd96
mody4z <modygaber23572357@gmail.com>
2025-11-11 13:56:24 +0200

    Some updates on CartRepository

--- Valora/Repositories/ICartRepository.cs
+++ Valora/Repositories/ICartRepository.cs
@@ -5,7 +5,8 @@ namespace Valora.Repositories
 {
     public interface ICartRepository : IRepository<Cart>
     {
-        void AddToCart(string UserID,int cartId, int productId, int quantity);
+
+        public Task AddToCart(string userId, int cartId, int productId, int quantity);
         CartDTO ShowTheCart(int cartId);
         public void RemoveFromCart(int cartId, int productId, int quantity);
 
