4305daca10b0a1cc7f78ccdf4f66c72c259ddbfd
MostafaBadr55 <mosstafabadr@gmail.com>
2025-11-11 09:57:52 +0200

    Adding category DbSet and make Repositories for Category and product models

--- Valora/Data/Context.cs
+++ Valora/Data/Context.cs
@@ -14,6 +14,7 @@ namespace Valora.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Cart> Carts { get; set; }
          public DbSet<CartItem> CartItems { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
